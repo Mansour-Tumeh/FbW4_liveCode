e5d9b4792d82d123bffbb5d993bb19943f81e05d
Safwan2025 <safwan.kherallah@digitalcareerinstitute.org>
2020-04-08 13:20:14 +0200

    08.04.2020 - 9.Express-Step-5-

--- 9-Express.js/express_second_day/index.js
+++ 9-Express.js/express_second_day/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
-const members = require('./Members');
 const logger = require('./middleware/logger')
 
 //initialize a variable called app as an instance object of express
@@ -9,38 +8,19 @@ const app = express();
 
 
 
-//use your middleware function
-// app.use(logger);
+//use your middleware functions
+//app.use(logger);
 
-//Get all Members
-app.get('/api/members', (req, res) => {
-    res.json(members);
-});
+//Member API Routes
 
-//Get a single Member
-app.get('/api/members/:id', (req, res) => {
-    // res.send(req.params.id)
-    // res.json(members.filter(member => {
-    //     return (member.id === parseInt(req.params.id))
-    // }))
-
-    // array.some(function(){condition}):boolean--returns true if one atleast follow the condition otherwise false
-    const found = members.some(member => (member.id === parseInt(req.params.id)))
-    if (found) {
-        res.json(members.filter(member => {
-            return (member.id === parseInt(req.params.id))
-        }))
-    } else {
-        res.status(404).json({
-            msg: `Member with id ${req.params.id} not found`
-        })
-    }
-});
+app.use('/api/members', require('./routes/api/members'))
 
 
 //Set a static folder : use method is for using a middlerware
 
 app.use(express.static('public'));
+
+
 
 dotenv.config();
 PORT = process.env.PORT;
